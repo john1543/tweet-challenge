acb3d176a676ecbb2cae328f4ca6d09c0044fbea
John Yadao <jyadao@academic.rrc.ca>
2013-11-04 10:23:11 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetChallenge::Application.config.secret_token = '067c556dabcd3d3b60cfe8b4fcd9903aaca29198965de8bd19a97e9b969d222a641f38a70a9c9f9e6648528f30ae04df6aae3e743ebae4efd6cb6b98b134eeaf'
